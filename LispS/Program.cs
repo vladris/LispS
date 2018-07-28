71ab52a22a2594e2e9c86ce65f19f6c91d2ead79
Vlad Riscutia <vladris@microsoft.com>
2018-07-28 14:14:50 -0700

    Add lambda support

--- LispS/Program.cs
+++ LispS/Program.cs
@@ -7,40 +7,41 @@ namespace LispS
 {
     abstract class SExpression
     {
-        public virtual bool IsAtom() => false;
-
-        public virtual bool IsNull() => false;
-
-        public override bool Equals(object obj)
-            => obj is SExpression ? this.IsNull() && ((SExpression)obj).IsNull() : false;
     }
 
-    class Atom<T> : SExpression
+    class Atom : SExpression
+    {
+        public static SExpression True = new Atom<string> { Value = "true" };
+
+        public static SExpression Nil = new Atom();
+
+        protected Atom() { }
+    }
+
+    class Atom<T> : Atom
     {
         public T Value { get; set; }
-
-        public override bool IsAtom() => true;
 
         public override bool Equals(object obj)
             => (obj as Atom<T>)?.Value.Equals(Value) ?? base.Equals(obj);
     }
 
+    class Name : Atom<string>
+    { }
+
     class Quote : SExpression
     {
         public SExpression Quoted { get; set; }
-
-        public override bool IsAtom() => Quoted.IsAtom();
     }
 
     class List : SExpression
     {
         public SExpression Head { get; set; }
         public SExpression Tail { get; set; }
-
-        public override bool IsAtom() => IsNull();
-
-        public override bool IsNull() => Head == null;
     }
+
+    class Lambda : List
+    { }
 
     class Context
     {
@@ -53,9 +54,14 @@ namespace LispS
 
         public Context Push() => new Context(this);
 
-        public void Store(string name, SExpression symbol) => Symbols[name] = symbol;
+        public SExpression Store(Name name, SExpression symbol)
+        {
+            Symbols[name.Value] = symbol;
+            return name;
+        }
 
-        public SExpression Resolve(string name) => Symbols.ContainsKey(name) ? Symbols[name] : Parent?.Resolve(name);
+        public SExpression Resolve(Name name)
+            => Symbols.ContainsKey(name.Value) ? Symbols[name.Value] : Parent?.Resolve(name);
     }
 
     class Evaluator
@@ -63,57 +69,75 @@ namespace LispS
         public static SExpression EvalExpr(SExpression expr, Context ctx)
             => Eval((dynamic)expr, ctx);
 
-        private static SExpression Eval<T>(Atom<T> atom, Context ctx)
-            => atom;
+        public static SExpression MakeBool(bool result)
+            => result ? Atom.True : Atom.Nil;
 
-        private static SExpression Eval(Quote quote, Context ctx)
-            => quote.Quoted;
+        private static SExpression Eval(Atom atom, Context ctx) => atom;
+
+        private static SExpression Eval(Name atom, Context ctx) => ctx.Resolve(atom);
+
+        private static SExpression Eval(Quote quote, Context ctx) => quote.Quoted;
 
         private static SExpression Eval(List list, Context ctx)
+            => EvalList((dynamic)list.Head, list.Tail, ctx);
+
+        public static SExpression EvalList(Name name, SExpression tail, Context ctx)
         {
-            if (list.IsNull()) return list;
+            var args = tail as List;
 
-            var scope = ctx.Push();
-            var name = (EvalExpr(list.Head, ctx) as Atom<string>).Value;
-            var args = list.Tail;
-
-            switch (name)
+            switch (name.Value)
             {
                 case "atom":
-                    return EvalExpr(args, ctx).IsAtom() ? new Atom<string> { Value = "T" } as SExpression: new List();
+                    return MakeBool(EvalExpr(tail, ctx) is Atom);
                 case "car":
-                    return EvalExpr((args as List).Head, ctx);
+                    return EvalExpr(args.Head, ctx);
                 case "cdr":
-                    return EvalExpr((args as List).Tail, ctx);
+                    return EvalExpr(args.Tail, ctx);
                 case "cons":
                     return new List {
-                        Head = EvalExpr((args as List).Head, ctx),
-                        Tail = EvalExpr((args as List).Tail, ctx)
+                        Head = EvalExpr(args.Head, ctx),
+                        Tail = EvalExpr(args.Tail, ctx)
                     };
+                case "eq":
+                    return MakeBool(EvalExpr(args.Head, ctx).Equals(EvalExpr(args.Tail, ctx)));
+                case "eval":
+                    return EvalExpr(args, ctx);
                 case "if":
-                    var expr = args as List;
-                    if (!EvalExpr(expr.Head, ctx).IsNull())
-                        return EvalExpr((expr.Tail as List).Head, ctx);
+                    if (EvalExpr(args.Head, ctx) != Atom.Nil)
+                        return EvalExpr((args.Tail as List).Head, ctx);
                     else
-                        return EvalExpr((expr.Tail as List).Tail, ctx);
+                        return EvalExpr((args.Tail as List).Tail, ctx);
                 case "quote":
-                    return new Quote { Quoted = args };
+                    return new Quote { Quoted = tail };
                 case "store":
-                    var storeSymbol = EvalExpr(list.Head, ctx) as Atom<string>;
-                    ctx.Store(storeSymbol.Value, list.Tail);
-                    return storeSymbol;
-                case "resolve":
-                    var callSymbol = EvalExpr(list.Head, ctx) as Atom<string>;
-                    return EvalExpr(ctx.Resolve(callSymbol.Value), ctx);
+                    return ctx.Store(EvalExpr(args.Head, ctx) as Name, EvalExpr(args.Tail, ctx));
                 default:
-                    throw new InvalidProgramException();
+                    return EvalList((dynamic)ctx.Resolve(name), args, ctx);
             }
+        }
+
+        public static SExpression EvalList(Lambda lambda, SExpression tail, Context ctx)
+        {
+            var scope = ctx.Push();
+            SExpression l = lambda;
+            while (l is Lambda)
+            {
+                scope.Store((l as Lambda).Head as Name, EvalExpr((tail as List).Head, ctx));
+                l = (l as Lambda).Tail;
+                tail = (tail as List).Tail;
+            }
+
+            return EvalExpr(l, scope);
         }
     }
 
     class Printer
     {
         public static string PrintExpr(SExpression expr) => Print((dynamic)expr);
+
+        private static string Print(Atom atom) => atom == Atom.True ? "T" :
+                                                  atom == Atom.Nil ? "()" :
+                                                  throw new InvalidDataException();
 
         private static string Print(Atom<int> number) => number.Value.ToString();
 
@@ -130,26 +154,62 @@ namespace LispS
         {
             var expr = new List
             {
-                Head = new Quote
-                {
-                    Quoted = new Atom<string> { Value = "cons" }
-                },
+                Head = new Name { Value = "cons" },
                 Tail = new List
                 {
                     Head = new List
                     {
-                        Head = new Atom<string> { Value = "cons" },
+                        Head = new Name { Value = "cons" },
                         Tail = new List
                         {
                             Head = new Atom<int> { Value = 1 },
-                            Tail = new Atom<int> { Value = 2 }
+                            Tail = new List
+                            {
+                                Head = new Name { Value = "cons" },
+                                Tail = new List
+                                {
+                                    Head = new Atom<int> { Value = 2 },
+                                    Tail = Atom.Nil
+                                }
+                            }
                         }
                     },
                     Tail = new Atom<int> { Value = 3 }
                 }
             };
 
+            var expr2 = new List
+            {
+                Head = new Lambda
+                {
+                    Head = new Name { Value = "x" },
+                    Tail = new Lambda
+                    {
+                        Head = new Name { Value = "y" },
+                        Tail = new List
+                        {
+                            Head = new Name { Value = "cons" },
+                            Tail = new List
+                            {
+                                Head = new Name { Value = "x" },
+                                Tail = new Name { Value = "y" }
+                            }
+                        }
+                    }
+                },
+                Tail = new List
+                {
+                    Head = new Atom<int> { Value = 1 },
+                    Tail = new List
+                    {
+                        Head = new Atom<int> { Value = 2 },
+                        Tail = Atom.Nil
+                    }
+                }
+            };
+
             Console.WriteLine(Printer.PrintExpr(Evaluator.EvalExpr(expr, Context.Make())));
+            Console.WriteLine(Printer.PrintExpr(Evaluator.EvalExpr(expr2, Context.Make())));
             Console.ReadLine();
         }
     }
